ba7fc7053c7bda38a7517a2e872b1e5da63cc150
Thiago William <thiago.will.silva@gmail.com>
2020-08-25 17:21:15 -0300

    working date

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -28,7 +28,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentRepository.create(provider, parsedDate);
+  const appointment = appointmentRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
